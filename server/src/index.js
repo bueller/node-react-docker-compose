76da868f82cbbdaddcfeb6042f69c11257ba5385
bueller <bueller@gmail.com>
2019-06-03 17:34:27 -0400

    little demo commit

--- server/src/index.js
+++ server/src/index.js
@@ -19,8 +19,9 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Alessandro!!!!'
   };
+  console.log('Alessandro');
   res.send(JSON.stringify(data, null, 2));
 });
 
