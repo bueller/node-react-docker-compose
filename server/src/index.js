1e32a0f8e28a0604fdac4db853f7bc420d7b6f90
bueller <bueller@gmail.com>
2019-06-06 18:12:10 -0400

    demo commit

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, mish!!!!'
   };
   res.send(JSON.stringify(data, null, 2));
+  conosle.log('missshhhaaa');
 });
 
 // All remaining requests return the React app, so it can handle routing.
