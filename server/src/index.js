660d018dc1fe354b0912f813ad4b0f964a469e8e
bueller <bueller@gmail.com>
2020-05-28 13:06:07 -0400

    Supreme and team demo commit

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Supreme and Team'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('Supreme and Team');
 });
 
 // All remaining requests return the React app, so it can handle routing.
