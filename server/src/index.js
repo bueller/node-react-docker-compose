90aee97fedbd142a430cc1a62e149ce5dbd55cf1
bueller <bueller@gmail.com>
2020-06-12 16:59:00 -0400

    Phil Demo Commit

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Phil Was here'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('Phill was here');
 });
 
 // All remaining requests return the React app, so it can handle routing.
