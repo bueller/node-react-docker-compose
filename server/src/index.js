1b538c00a3bc53ef52dfec4d8f65ae933c6df250
bueller <bueller@gmail.com>
2020-06-19 13:23:34 -0400

    adfasdf

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!adfdaf'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('adsf');
 });
 
 // All remaining requests return the React app, so it can handle routing.
