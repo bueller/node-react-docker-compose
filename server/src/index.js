31ffb4dc2fdc5b8cc2b7a34c6c1866326c7f1e4c
bueller <bueller@gmail.com>
2020-06-30 10:29:45 -0400

    demo test commit

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!randoi'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('ahoy');
 });
 
 // All remaining requests return the React app, so it can handle routing.
