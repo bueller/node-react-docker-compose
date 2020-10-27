4dd55b6602ff193d4a1e55eaa151ee31f55e63ec
bueller <bueller@gmail.com>
2020-10-27 14:21:33 -0400

    Drata Demo Commit

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Adam was here'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('Troy was lurking here');
 });
 
 // All remaining requests return the React app, so it can handle routing.
