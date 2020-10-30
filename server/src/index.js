c4ac143e4b4afb91ef4ae338a42a85869f89d000
bueller <bueller@gmail.com>
2020-10-30 12:24:19 -0400

    Saar demo commit

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Saar was here'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('Saar was alllso hereeee');
 });
 
 // All remaining requests return the React app, so it can handle routing.
