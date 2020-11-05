e9ccb9b5dff782dd4ee32f9e23d841202150bac7
bueller <bueller@gmail.com>
2020-11-05 15:18:11 -0500

    Demo for Misha Burk

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Misha was here'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('misha was in here');
 });
 
 // All remaining requests return the React app, so it can handle routing.
