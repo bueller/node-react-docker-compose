c9e1d7b24b197c69f69ab2dbc747757e17baae6e
bueller <bueller@gmail.com>
2020-11-20 14:51:26 -0500

    Artem demo commit

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Artem was here checking things out'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('Artem was also int he console');
 });
 
 // All remaining requests return the React app, so it can handle routing.
