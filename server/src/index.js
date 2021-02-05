3dd6190ed1f7443fd95ed33d69f2583c9ac65c2b
bueller <bueller@gmail.com>
2021-02-05 15:11:41 -0500

    Demo Ethan Jackson

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Ethan Jackson'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('Ethan Jackson was in the console');
 });
 
 // All remaining requests return the React app, so it can handle routing.
