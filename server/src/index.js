73c2d6d8575c86f16d06914921b85f1c97b16a28
bueller <bueller@gmail.com>
2021-03-04 14:44:36 -0500

    Demo Commit Jeff See

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Jeff See was here'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('Jeff was also in conosle');
 });
 
 // All remaining requests return the React app, so it can handle routing.
