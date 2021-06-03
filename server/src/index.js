34f9158edb8468353d8d25e777308610aa255203
Bueller <bueller@gmail.com>
2021-06-03 17:28:07 -0400

    Demo commit for Jared

--- server/src/index.js
+++ server/src/index.js
@@ -19,9 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Jared was here'
   };
   res.send(JSON.stringify(data, null, 2));
+  console.log('Jared Ritter was in the console');
 });
 
 // All remaining requests return the React app, so it can handle routing.
