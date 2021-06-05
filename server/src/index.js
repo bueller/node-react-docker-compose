f18e98b604b96e9b443c39bb1e8f4a8280a49861
Bueller <bueller@gmail.com>
2021-06-04 23:36:20 -0400

    second test for ttl after i set correct one i thin

--- server/src/index.js
+++ server/src/index.js
@@ -19,7 +19,7 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!build keep alive test 2'
   };
   res.send(JSON.stringify(data, null, 2));
 });
