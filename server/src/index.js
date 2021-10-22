73509a8e7c5e01030866d0a21cc65642745f90f6
Bueller <bueller@gmail.com>
2021-10-22 13:20:33 -0400

    Demo for Hy-Vee

--- server/src/index.js
+++ server/src/index.js
@@ -19,10 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Hy-Vee was here'
   };
   res.send(JSON.stringify(data, null, 2));
-  console.log('Peter was in the console');
+  console.log('Hy-Vee was in the consoles');
 });
 
 // All remaining requests return the React app, so it can handle routing.
