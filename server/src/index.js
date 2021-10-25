b2be58f16c2f382fc543bacfb99ac5e6f4c8702f
Bueller <bueller@gmail.com>
2021-10-25 14:12:37 -0400

    Demo for Ashley + Ben from coder

--- server/src/index.js
+++ server/src/index.js
@@ -19,10 +19,10 @@ app.use(express.static(CLIENT_BUILD_PATH));
 app.get('/api', (req, res) => {
   res.set('Content-Type', 'application/json');
   let data = {
-    message: 'Hello world, Woooooeeeee!!!!'
+    message: 'Hello world, Woooooeeeee!!!!Coder Peeps were here'
   };
   res.send(JSON.stringify(data, null, 2));
-  console.log('Peter was in the console');
+  console.log('Coder Peeps were in the console');
 });
 
 // All remaining requests return the React app, so it can handle routing.
