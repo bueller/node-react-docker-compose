4e936b2604cbfd80aefdf38b8290b1f894a04a3f
Bueller <bueller@gmail.com>
2025-08-14 15:27:57 -0400

    Update App.js breaking for zach

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './compiled/App.css';
+import './compiled/Appsssss.css';
 
 class App extends Component {
   constructor() {
